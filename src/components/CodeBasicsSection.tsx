024b50b090c4c0e403829dc9c47a9937cd44ffac
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-06-05 19:56:17 +0200

    added code snippets

--- src/components/CodeBasicsSection.tsx
+++ src/components/CodeBasicsSection.tsx
@@ -6,7 +6,7 @@ function CodeBasicsSection() {
       className="p-8 dark:bg-gray-800 dark:text-gray-100 scroll-mt-24"
       id="code"
     >
-      <div className="container mx-auto p-4 mb-6 space-y-2 text-center">
+      <div className="container mx-auto p-4space-y-2 text-center">
         <h2
           className="text-4xl font-bold mb-4 relative
           before:w-44 before:h-1 before:rounded-lg before:bg-violet-400 before:absolute before:-top-8 before:left-1/2 before:-translate-x-1/2"
@@ -17,7 +17,30 @@ function CodeBasicsSection() {
           Zmienne, tablice, instrukcje warunkowe, pętle
         </p>
       </div>
-      <CodeSnippet />
+      <h3 className="text-3xl my-6 text-center">Zmienne</h3>
+      <CodeSnippet
+        code={{
+          js: '//string\nlet string = "Hello world!"\n//int\nlet int = 123\n//boolean\nlet boolean = false\n//stała \nconst zmienna = "Stała zmienna"\n//Wypisywanie danych \nconsole.log(zmienna)',
+          php: '//string\n$string = "Hello world!";\n//int\n$int = 123;\n//boolean\n$zmienna_boolean = true;\n//stała \ndefine("STALA_ZMIENNA", "Ta treść nigdy nie może być zmieniona");\n//Wypisywanie danych \necho STALA_ZMIENNA;',
+          cpp: '#include <iostream>\nusing namespace std;\n\nint main() {\n  //string\n  string s1 = "Hello world!";\n  //int\n  int liczba = 123;\n  //boolean\n  bool b = false;\n  //stała \n  const string stala = "Stała zmienna";\n  //Wypisywanie danych\n  cout<<stala;\n  return 0;\n}',
+        }}
+      />
+      <h3 className="text-3xl my-6 text-center">Tablice</h3>
+      <CodeSnippet
+        code={{
+          js: 'let array = []; \narray.push(12) \narray.pop()',
+          php: '$array = [];',
+          cpp: '#include <iostream>\nusing namespace std;\n\nint main() {\n  int tablica_liczb[10];\n  return 0;\n}',
+        }}
+      />
+      <h3 className="text-3xl my-6 text-center">Instrukcje warunkowe - if</h3>
+      <CodeSnippet
+        code={{
+          js: '//todo: js if',
+          php: '//todo: php if',
+          cpp: '#include <iostream>\nusing namespace std;\n\nint main() {\n  //todo: c++ if\n  return 0;\n}',
+        }}
+      />
     </section>
   );
 }
--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -1,68 +1,94 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState } from 'react';
-import { LuClipboard } from 'react-icons/lu';
+import { LuClipboard, LuClipboardCheck } from 'react-icons/lu';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { tomorrowNight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function CodeSnippet() {
-  const [selectedLang, setSelectedLang] = useState<'JS' | 'PHP' | 'CPP'>('JS');
-  const codeString =
-    'console.log("Hello World"); \nfunction Test(){\n  return 12;\n}';
+interface CodeSnippetProps {
+  code: {
+    php: string;
+    cpp: string;
+    js: string;
+  };
+}
+
+function CodeSnippet({ code }: CodeSnippetProps) {
+  const [selectedLang, setSelectedLang] = useState<'js' | 'php' | 'cpp'>('js');
+  const [codeCopied, setCodeCopied] = useState(false);
+
+  const copyCode = () => {
+    navigator.clipboard.writeText(code[selectedLang]).then(() => {
+      setCodeCopied(true);
+      setTimeout(() => {
+        setCodeCopied(false);
+      }, 1500);
+    });
+  };
+
   return (
     <div className="container mx-auto mb-10 relative">
       <div className="flex flex-nowrap dark:bg-gray-800 dark:text-gray-100">
         <p
           className={`px-5 py-3 -mb-0.5 dark:border-gray-100 cursor-pointer ${
-            selectedLang === 'JS'
+            selectedLang === 'js'
               ? 'border border-b-0 bg-code rounded-t-lg text-gray-50'
               : 'text-gray-400'
           }`}
           onClick={() => {
-            setSelectedLang('JS');
+            setSelectedLang('js');
           }}
         >
           JS
         </p>
         <p
           className={`px-5 py-3 -mb-0.5 dark:border-gray-100 cursor-pointer ${
-            selectedLang === 'PHP'
+            selectedLang === 'php'
               ? 'border border-b-0 bg-code rounded-t-lg text-gray-50'
               : 'text-gray-400'
           }`}
           onClick={() => {
-            setSelectedLang('PHP');
+            setSelectedLang('php');
           }}
         >
           PHP
         </p>
         <p
           className={`px-5 py-3 -mb-0.5 dark:border-gray-100 cursor-pointer ${
-            selectedLang === 'CPP'
+            selectedLang === 'cpp'
               ? 'border border-b-0 bg-code rounded-t-lg text-gray-50'
               : 'text-gray-400'
           }`}
           onClick={() => {
-            setSelectedLang('CPP');
+            setSelectedLang('cpp');
           }}
         >
           C++
         </p>
-        <div className="px-5 py-3 -mb-0.5 ml-auto hover:text-violet-400 dark:border-gray-100 cursor-pointer border border-b-0 bg-code rounded-t-lg text-gray-400">
-          <LuClipboard className="text-inherit w-6 h-6" />
+        <div
+          className="px-5 py-3 -mb-0.5 ml-auto hover:text-violet-400 dark:border-gray-100 cursor-pointer border border-b-0 bg-code rounded-t-lg text-gray-400"
+          role="button"
+          tabIndex={0}
+          onClick={copyCode}
+        >
+          {codeCopied ? (
+            <LuClipboardCheck className="text-inherit w-6 h-6 text-green-500" />
+          ) : (
+            <LuClipboard className="text-inherit w-6 h-6" />
+          )}
         </div>
       </div>
       <div
         className={`p-4 rounded-lg border border-gray-100 rounded-se-none bg-code ${
-          selectedLang === 'JS' ? 'rounded-ss-none' : ''
+          selectedLang === 'js' ? 'rounded-ss-none' : ''
         }`}
       >
         <SyntaxHighlighter
-          language="javascript"
+          language={selectedLang === 'js' ? 'javascript' : selectedLang}
           style={tomorrowNight}
           showLineNumbers
         >
-          {codeString}
+          {code[selectedLang]}
         </SyntaxHighlighter>
       </div>
     </div>
