4e686f838a6d3a4b398289313841f51672c8cfbc
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-06-06 18:28:53 +0200

    typo

--- src/components/CodeBasicsSection.tsx
+++ src/components/CodeBasicsSection.tsx
@@ -6,7 +6,7 @@ function CodeBasicsSection() {
       className="p-8 dark:bg-gray-800 dark:text-gray-100 scroll-mt-24"
       id="code"
     >
-      <div className="container mx-auto p-4space-y-2 text-center">
+      <div className="container mx-auto p-4 space-y-2 text-center">
         <h2
           className="text-4xl font-bold mb-4 relative
           before:w-44 before:h-1 before:rounded-lg before:bg-violet-400 before:absolute before:-top-8 before:left-1/2 before:-translate-x-1/2"
