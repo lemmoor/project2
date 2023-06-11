309a9e3ae53bb46d4099f8f5e04075644a614b27
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-06-11 21:13:41 +0200

    fix overflowing line on mobile

--- src/components/IntroBanner.tsx
+++ src/components/IntroBanner.tsx
@@ -29,7 +29,7 @@ function IntroBanner() {
     >
       <h1 className="mb-1 text-4xl md:text-6xl overflow-y-hidden dark:text-gray-900 w-64 md:w-[27rem] py-36">
         Podstawy&nbsp;
-        <span className="inline-flex font-mono h-20 pt-2 overflow-x-hidden animate-type whitespace-nowrap text-brand-accent will-change-transform">
+        <span className="inline-flex font-mono h-20 pt-2 overflow-x-hidden animate-type whitespace-nowrap text-brand-accent will-change-transform line-clamp-1">
           {textContent}
         </span>
         <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform" />
